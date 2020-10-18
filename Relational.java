ae2166beb71bbc2acfbc6822b781aa3fcc6c496e
maidiyangsa <57142580+maidiyangsa@users.noreply.github.com>
2020-10-18 14:19:33 +0800

    relational

---
+++ Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * @author Beta Three
+ * NAMA     : ADRIAN MAIDIYANGSA
+ * KELAS    : IF-1
+ * NIM      : 10119029
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
